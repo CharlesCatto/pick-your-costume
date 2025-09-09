7d3a37c9060dfa377aa3283b65173d243dafba16
Charles Catto <charlescatto@outlook.fr>
2025-09-09 10:56:41 +0200

    Cleanup MySQL, fix PostgreSQL types and connection

--- server/src/database/client.ts
+++ server/src/database/client.ts
@@ -1,11 +1,11 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
-	port: Number(process.env.DB_PORT) || 5432,
+	host: process.env.PGHOST,
+	user: process.env.PGUSER,
+	password: process.env.PGPASSWORD,
+	database: process.env.PGDATABASE,
+	port: Number(process.env.PGPORT) || 5432,
 	ssl:
 		process.env.NODE_ENV === "production"
 			? { rejectUnauthorized: false }
